8b51c949cf3be61a9ee6d854549266f4df4974b5
Thiarasak Khamyan <thiarasak@the4n.com>
2022-02-06 14:24:26 +0700

    make it better

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from logging import DEBUG
-from flask import Flask, render_template, url_for
-
+from flask import Flask, render_template, url_for, redirect, jsonify, json, request, session
 app = Flask(__name__)
 app.debug = True
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', status=session['status'])
+
+@app.route('/status', methods=['POST'])
+def status_post():
+    postJson = request.get_json()
+    status = "online"
+    if postJson:
+        status = postJson['status']
+        session['status'] = status
+    return jsonify(status)
 
 if __name__ == '__main__':
-    app.run(host='192.168.10.2')
+    app.run(host='192.168.10.6')
